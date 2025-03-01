6833dd029c1c35350dcd47561936f293887b4592
Julien BONNET <julienbonnet50@gmail.com>
2025-03-01 17:19:25 +0100

    :wrench: - Fix host app.py conf

--- backend/app.py
+++ backend/app.py
@@ -107,4 +107,4 @@ def simulate_draft():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=appConfig.port)
+    app.run(debug=True, port=appConfig.port)
