874f18cc603c6861ae60eb59961611dd5d1bc56b
Julien BONNET <julienbonnet50@gmail.com>
2025-03-01 16:48:34 +0100

    :bug: - Reverse conf

--- frontend/brawl-stars-draft-simulator/next.config.js
+++ frontend/brawl-stars-draft-simulator/next.config.js
@@ -1,23 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.brawlify.com",
-        pathname: "/brawlers/borders/*", // Allow images from this external domain
-      },
-    ],
-  },
-  experimental: {
-    turbo: false, // Disable Turbopack to avoid Webpack conflict
-  },
-  webpack: (config) => {
-    config.resolve.alias["react-server-dom-webpack/server.edge"] = require.resolve(
-      "react-server-dom-webpack/server.edge"
-    );
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+// next.config.js
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'cdn.brawlify.com', // External domain
+          pathname: '/brawlers/borders/*', // Optional, specify path pattern if needed
+        },
+      ],
+    },
+  };
+  
